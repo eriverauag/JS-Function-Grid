48bcb7ad464ff7e4878bd39ca392ee3dc20e8eac
ethanriverauag <ethanrivera@uagateway.org>
2016-12-07 21:11:14 +0000

    Created random colored square

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //Write your function calls below this line
+function getRandomInt(min,max) {
+    min = Math.ceil(min);
+    max = Math.ceil(max);
+    return Math.floor(Math.random() * (max - min)) +min;
+}
+setColor(getRandomInt(0,9),getRandomInt(0,9),"red");
